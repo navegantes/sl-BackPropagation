c09636d64d50aa4ec52205078e62e9dd30ac4e6f
navegantes <raphaelnavegantes@gmail.com>
2018-04-17 22:47:00 -0300

    17-04-18-22-46

--- backprop.py
+++ backprop.py
@@ -15,31 +15,51 @@ class sl_BackProp:
     '''
     '''
     
-    def __init__(self, n_input = 1, n_neuron = [1], n_Hlayer = 1, n_output = 1, coefLearn = 0.01 ):
+    def __init__(self, n_input = 1, n_neuron = 1, n_output = 1, actFunc='linear', coefLearn = 0.01 ):
 
-        self.inLayer = np.zeros(n_input)
-        self.hLayers = np.zeros(n_Hlayer)
-        self.outLayer = np.zeros(n_output)
-        self.n_Input = n_input
-        self.n_HiddenLayers = n_Hlayer
+        self.n_Input  = n_input
         self.n_Output = n_output
+        #self.n_HiddenLayers = n_Hlayer
+
+        self.inLayer  = np.zeros(self.n_Input)
+        self.nNeurons = n_neuron
+        self.hLayers  = np.zeros(self.nNeurons)
+        self.outNet = np.zeros(self.n_Output)
+        #self.bias = 
 
         self.coefLearn = coefLearn
-        self.Weigths = [ np.random.rand(self.n_Input, self.n_HiddenLayers), np.random.rand(self.n_HiddenLayers, self.n_Output) ]
+        self.Weigths = [ np.random.rand(self.n_Input, self.nNeurons), np.random.rand(self.nNeurons, self.n_Output) ]
 
-        self.actFunc = {'linear':0, 'sigmoid':1, 'hipertan':2, 'arctan':3}
+        activationFunc = {'linear':0, 'sigmoid':1, 'hipertan':2, 'arctan':3} 
+        self.actFunc = activationFunc[actFunc]
+
+        self.filepath = ''
+        self.Data = []
+        self.output = []
         
     def readData(self):
+        
         self.filepath = askopenfilename(parent=root, title="Choose data set!").__str__()
-        file = open('self.filepath','r')
+        file = open(self.filepath,'r')
         self.Data = file.readlines()
+
+        file.close()
 
     def normalize(self):
         pass
 
     def trainet(self):
+        
+        H0 = np.dot(self.inLayer, self.Weigths[0]) #+ bias[0]
+        self.hLayers = 1. / (1 + np.exp(H0))
 
-        np.dot(self.inLayer, )
+        out = np.dot(self.hLayers, self.Weigths[1]) #+ bias[1]
+        self.outNet = 1. / (1 + np.exp(out))
+
+        err = self.output - self.outNet
 
     def runet(self):
         pass
+
+if __name__ == "__main__":
+    pass
