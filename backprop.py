2b9bf8b6a27bcf10806a40e4e9128203e76d1391
Navegantes <raphaelnavegantes@gmail.com>
2018-04-25 13:35:52 -0300

    25-04-18-13-35

--- backprop.py
+++ backprop.py
@@ -14,8 +14,8 @@ root.withdraw()
 class sl_BackProp:
     '''
     '''
-    
-    def __init__(self, n_input = 1, n_neuron = 1, n_output = 1, actFunc='linear', coefLearn = 0.01 ):
+
+    def __init__(self, n_input = 1, n_neuron = 1, n_output = 1, actFunc='sigmoid', coefLearn = 0.01 ):
 
         self.n_Input  = n_input
         self.n_Output = n_output
@@ -23,22 +23,23 @@ class sl_BackProp:
 
         self.inLayer  = np.zeros(self.n_Input)
         self.nNeurons = n_neuron
-        self.hLayers  = np.zeros(self.nNeurons)
-        self.outNet = np.zeros(self.n_Output)
-        #self.bias = 
+        self.hLayer  = np.zeros(self.nNeurons)
+        self.outLayer = np.zeros(self.n_Output)
+        #self.bias =
 
         self.coefLearn = coefLearn
         self.Weigths = [ np.random.rand(self.n_Input, self.nNeurons), np.random.rand(self.nNeurons, self.n_Output) ]
+        self.dW = [ np.zeros([self.n_Input, self.nNeurons]), np.zeros(self.nNeurons, self.n_Output) ]
 
-        activationFunc = {'linear':0, 'sigmoid':1, 'hipertan':2, 'arctan':3} 
-        self.actFunc = activationFunc[actFunc]
+        activation = {'linear':0, 'sigmoid':1, 'hipertan':2, 'arctan':3}
+        self.actFunc = activation[actFunc]
 
         self.filepath = ''
-        self.Data = []
-        self.outpattern = []
-        
+        self.inData = []
+        self.output = []
+
     def readData(self):
-        
+
         self.filepath = askopenfilename(parent=root, title="Choose data set!").__str__()
         file = open(self.filepath,'r')
         self.Data = file.readlines()
@@ -50,13 +51,29 @@ class sl_BackProp:
 
     def trainet(self):
         
+        # Forward direction
+        H0 = np.zeros(self.nNeurons)
+        for i in range(self.nNeurons):
+            H0[i] = np.sum( self.inLayer*self.Weigths[0][:,i] ) # + bias
+
         H0 = np.dot(self.inLayer, self.Weigths[0]) #+ bias[0]
-        self.hLayers = 1. / (1 + np.exp(H0))
 
-        out = np.dot(self.hLayers, self.Weigths[1]) #+ bias[1]
-        self.outNet = 1. / (1 + np.exp(out))
+        self.hLayer = 1. / (1 + np.exp(-1*H0))
 
-        err = self.output - self.outNet
+        out = np.dot(self.hLayer, self.Weigths[1]) #+ bias[1]
+        self.outLayer = 1. / (1 + np.exp(-1*out))
+
+        err = self.output - self.outLayer
+
+        # Backward direction
+        #Output layer gradient
+        oGrad = err*self.outLayer*(1-self.outLayer)
+
+        #Updating weigths
+        dW[1] = self.coefLearn*oGrad*self.hLayer
+
+        #Hidden layer gradient
+        hGrad = 
 
 
     def runet(self):
