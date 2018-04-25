855fe11c7b085a8e65d652a14913d845dc054636
Navegantes <raphaelnavegantes@gmail.com>
2018-04-25 17:26:45 -0300

    25-04-18-17-26

--- backprop.py
+++ backprop.py
@@ -19,6 +19,7 @@ class sl_BackProp:
 
         self.n_Input  = n_input
         self.n_Output = n_output
+        self.nEpochs  = 100
         #self.n_HiddenLayers = n_Hlayer
 
         self.inLayer  = np.zeros(self.n_Input)
@@ -28,23 +29,34 @@ class sl_BackProp:
         #self.bias =
 
         self.coefLearn = coefLearn
-        self.Weigths = [ np.random.rand(self.n_Input, self.nNeurons), np.random.rand(self.nNeurons, self.n_Output) ]
-        self.dW = [ np.zeros([self.n_Input, self.nNeurons]), np.zeros(self.nNeurons, self.n_Output) ]
+        self.IW = [ np.random.rand(self.n_Input, self.nNeurons), np.random.rand(self.nNeurons, self.n_Output) ]
+        self.dW = [ np.zeros([self.n_Input, self.nNeurons]), np.zeros([self.nNeurons, self.n_Output]) ]
+        self.Grad = [ np.zeros([1, self.nNeurons]), np.zeros([1, self.n_Output]) ]
 
         activation = {'linear':0, 'sigmoid':1, 'hipertan':2, 'arctan':3}
         self.actFunc = activation[actFunc]
 
         self.filepath = ''
-        self.inData = []
-        self.output = []
+        self.inData, self.output = self.readData()
 
     def readData(self):
 
         self.filepath = askopenfilename(parent=root, title="Choose data set!").__str__()
         file = open(self.filepath,'r')
-        self.Data = file.readlines()
+        #stream = file.read()
+        strm1 = file.read().split('\n')
+        #[ln. for ln in strm1]
+        inData = np.zeros([4, len(strm1)-1])
+        output = np.zeros([1, len(strm1)-1])
+        #print self.inData[:,0]
+        # print strm1[0].split(',')[0:3]
+
+        for ln in range(len(strm1)-1):
+            inData[:, ln] = np.array( [float(i) for i in strm1[ln].split(',')])[0:4]
+            output[0][ln] = np.array( [float(i) for i in strm1[ln].split(',')])[-1]
 
         file.close()
+        return [inData, output]
 
     def normalize(self):
         pass
@@ -52,15 +64,15 @@ class sl_BackProp:
     def trainet(self):
         
         # Forward direction
-        H0 = np.zeros(self.nNeurons)
-        for i in range(self.nNeurons):
-            H0[i] = np.sum( self.inLayer*self.Weigths[0][:,i] ) # + bias
+        # H0 = np.zeros(self.nNeurons)
+        # for i in range(self.nNeurons):
+        #     H0[i] = np.sum( self.inLayer*self.Weigths[0][:,i] ) # + bias
 
-        H0 = np.dot(self.inLayer, self.Weigths[0]) #+ bias[0]
+        H0 = np.dot(self.inLayer, self.IW[0]) #+ bias[0]
 
         self.hLayer = 1. / (1 + np.exp(-1*H0))
 
-        out = np.dot(self.hLayer, self.Weigths[1]) #+ bias[1]
+        out = np.dot(self.hLayer, self.IW[1]) #+ bias[1]
         self.outLayer = 1. / (1 + np.exp(-1*out))
 
         err = self.output - self.outLayer
@@ -70,14 +82,15 @@ class sl_BackProp:
         oGrad = err*self.outLayer*(1-self.outLayer)
 
         #Updating weigths
-        dW[1] = self.coefLearn*oGrad*self.hLayer
+        self.dW[1] = self.coefLearn*oGrad*self.hLayer
 
         #Hidden layer gradient
-        hGrad = 
+        #hGrad = 
 
 
     def runet(self):
         pass
 
 if __name__ == "__main__":
-    pass
+    
+    net = sl_BackProp(n_input=4, n_neuron=5, n_output=3)
